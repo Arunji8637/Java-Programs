989ae8b5f7d8b0f1a3c4d1c1ad8b126821c9aa31
Arunji8637 <155039930+Arunji8637@users.noreply.github.com>
2024-01-03 19:30:44 +0530

    Add files via upload

---
+++ Java Program/Voter Eligibility/VoterEligibility.java
@@ -0,0 +1,13 @@
+class VoterEligibility{
+public static void main(String args[]){
+int voterAge=30;
+int eligiblityAge=18;
+if(voterAge>=eligiblityAge)
+{
+System.out.println("The Person is Eligible to Vote.");
+}
+else{
+System.out.println("The Person is Not Eligible to Vote.");
+}
+}
+}
