a231c87d78496c39f9a9b90ac41130dee5e32191
Arunji8637 <155039930+Arunji8637@users.noreply.github.com>
2024-01-11 08:41:00 +0530

    Add files via upload

---
+++ Java Program/Command Line Method to find Even/CommandLineEven.java
@@ -0,0 +1,11 @@
+class CommandLineEven{
+public static void main(String args[]){
+int n=Integer.parseInt(args[0]);
+if(n%2==0){
+System.out.println("Given number is Even");
+}
+else{
+System.out.println("Given Number is not Even");
+}
+}
+}
