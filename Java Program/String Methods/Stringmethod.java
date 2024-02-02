37d49f99aa8c0e003d1731d62903396dd34a53cd
Arunji8637 <155039930+Arunji8637@users.noreply.github.com>
2024-02-02 21:54:15 +0530

    Add files via upload

---
+++ Java Program/String Methods/Stringmethod.java
@@ -0,0 +1,11 @@
+class Stringmethod{
+public static void main(String args[]){
+String str="Hello";
+String str1="World";
+System.out.println(str.charAt(1));
+System.out.println(str.concat(str1));
+System.out.println(str.length());
+System.out.println(str.toUpperCase());
+System.out.println(str.toLowerCase());
+}
+}
